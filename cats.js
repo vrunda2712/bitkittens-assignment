6b5e25de1dc572fef7c66a69564eb854ad41fdbb
vrunda2712 <angy.v27@gmail.com>
2017-07-11 16:50:35 -0400

    Adding Cats to DOM

--- cats.js
+++ cats.js
@@ -6,6 +6,24 @@ document.addEventListener('DOMContentLoaded',function() {
       url: 'http://bitkittens.herokuapp.com/cats.json',
       method: 'GET',
       dataType: 'json',
+    }).done(function(data) {
+
+      catBox(data.cats[0], 1);
+      catBox(data.cats[1], 2);
+      catBox(data.cats[2], 3);
     });
   });
 });
+
+function catBox(cat, number) {
+  // var selector = '#cat1.cat-box';
+  var selector = '#cat' + number + '.cat-box';
+
+  var catBox = document.querySelector(selector);
+  var kittenPicSrc = cat.photo;
+  var kittenPicName = cat.name;
+  var img = document.createElement('img');
+  img.src = kittenPicSrc;
+  img.alt = kittenPicName;
+  catBox.appendChild(img);
+}
