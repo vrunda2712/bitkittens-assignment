39407fc2c9aca67009adf7d34a0943cf29069a26
vrunda2712 <angy.v27@gmail.com>
2017-07-11 15:50:36 -0400

    Making the AJAX request

--- cats.js
+++ cats.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded',function() {
   var summonKitties = document.querySelector('.summon-cats');
 
   summonKitties.addEventListener('click',function(){
-    console.log('Yay.....!!')
+    $.ajax( {
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      dataType: 'json',
+    });
   });
 });
