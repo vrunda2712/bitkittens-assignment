01e6d8866dbec81488d7bfc854693cc737f28cce
vrunda2712 <angy.v27@gmail.com>
2017-07-11 15:36:30 -0400

    Setting up the event handler

---
+++ cats.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded',function() {
+  var summonKitties = document.querySelector('.summon-cats');
+
+  summonKitties.addEventListener('click',function(){
+    console.log('Yay.....!!')
+  });
+});
